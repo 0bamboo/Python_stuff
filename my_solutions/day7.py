4ab286f04770e4178c3dae3fb61f745eacbb0803
Paayk <itsabdennacerama@gmail.com>
2020-11-25 20:38:37 +0100

    day 8

--- my_solutions/day7.py
+++ my_solutions/day7.py
@@ -10,4 +10,29 @@ div = Div_by_7()
 div.div(int(input('> ')))
 
 # ================ Q21 ===================
+import math
+
 print('--------------- Q21 ----------------')
+lis = []
+x, y, dist = 0, 0, 0
+while 1:
+    steps = input().split()
+    if not steps:
+        break
+    lis.append(steps)
+
+for item in lis:
+    print(item[0])
+    if item[0].upper() == "UP":
+        x += int(item[1])
+    if item[0].upper() ==  'DOWN':
+        x -= int(item[1])
+    if item[0].upper() == "RIGHT":
+        y += int(item[1])
+    if item[0].upper() ==  'LEFT':
+        y -= int(item[1])
+print(x,y)
+dist = math.sqrt(x ** 2 + y ** 2)
+print(dist)
+
+# ===================== DONE =================
