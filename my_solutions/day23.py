8c1576568aa51a8b435268c7e41622e2d8e2b120
paayk <itsabdennacerama@gmail.com>
2020-12-10 23:26:22 +0100

    not yet

--- my_solutions/day23.py
+++ my_solutions/day23.py
@@ -1,30 +1,81 @@
 # ============================== Q97 ========================
 print('======================== Q97 ==========================')
+n = int(input('Enter a number : '))
+alpha = "abcdefjhgklmnopqrstvwxyz"
+index = n - 1
+num_ = (n - 1) * 4
+tmp = ""
+tmp += '-' * (num_ // 2)
+tmp += alpha[index]
+tmp += '-' * (num_ // 2)
 
-# =================== DONE ==================================
-# ======================== Q98 ===========================
-print('====================== Q98 =========================')
-import calendar as cl
-lis = input('Enter the date : ').split()
-lis_days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
-day = cl.weekday(int(lis[-1]), int(lis[1]), int(lis[0]))
-print(day)
-print('The day is : {}'.format(lis_days[day]))
+#  the method that im going to use is like this :
+# first im gonna fill all the blocks with hyphens then .... see you tomorrow
 
-# =========================== DONE =========================
-# ============================= Q99 ===========================
-print('========================= Q99 ==========================')
-first_set = list(map(int, input('First set : ').split()))
-second_set = list(map(int, input('Second set : ').split()))
-output = []
-for item in first_set:
-    if item not in second_set:
-        output.append(item)
-for item in second_set:
-    if item not in first_set:
-        output.append(item)
-output.sort()
-# print('The symmetric difference is : {}'.format(output))
-for item in output:
-    print(item , end = ' ')
-# =================================  THE END =================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # =================== DONE ==================================
+# # ======================== Q98 ===========================
+# print('====================== Q98 =========================')
+# import calendar as cl
+# lis = input('Enter the date : ').split()
+# lis_days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+# day = cl.weekday(int(lis[-1]), int(lis[1]), int(lis[0]))
+# print(day)
+# print('The day is : {}'.format(lis_days[day]))
+
+# # =========================== DONE =========================
+# # ============================= Q99 ===========================
+# print('========================= Q99 ==========================')
+# first_set = list(map(int, input('First set : ').split()))
+# second_set = list(map(int, input('Second set : ').split()))
+# output = []
+# for item in first_set:
+#     if item not in second_set:
+#         output.append(item)
+# for item in second_set:
+#     if item not in first_set:
+#         output.append(item)
+# output.sort()
+# # print('The symmetric difference is : {}'.format(output))
+# for item in output:
+#     print(item , end = ' ')
+# # =================================  THE END =================================
