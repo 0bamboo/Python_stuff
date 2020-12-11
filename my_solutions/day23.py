6486a3ae4dd1c3cd8b02ff7268656fef2058574c
paayk <itsabdennacerama@gmail.com>
2020-12-11 22:52:10 +0100

    ddd

--- my_solutions/day23.py
+++ my_solutions/day23.py
@@ -1,13 +1,57 @@
 # ============================== Q97 ========================
+import sys
 print('======================== Q97 ==========================')
 n = int(input('Enter a number : '))
+if n > 24 or n < 1:
+    print('YOU NEED TO ENTER A NUMBER BETWEEN 1=< N =< 24 ')
+    sys.exit()
+
+ret_lis = []
+ret_str = ''
 alpha = "abcdefjhgklmnopqrstvwxyz"
 index = n - 1
-num_ = (n - 1) * 4
-tmp = ""
-tmp += '-' * (num_ // 2)
-tmp += alpha[index]
-tmp += '-' * (num_ // 2)
+num_hphn = (n - 1) * 4 + 1
+lis = []
+i = 1
+while i <= n:
+    lis.append('-' * num_hphn)
+    i += 1
+sign = 1
+for i, d in enumerate(lis[0]):
+    if i % 2 == 0:
+        ret_str += alpha[index]
+        index -= sign
+        if index == -1:
+            sign = -1
+            index += 2
+        continue
+    ret_str += d
+ret_lis.append(ret_str)
+ret_str = ''
+j = index - 1
+count = 2
+for ind,item in enumerate(lis):
+    if ind == 0:
+        continue
+    ret_str += '-' * count
+    for x in range(index + 1, num_hphn):
+        if x % 2 == 0:
+            ret_str += alpha[j]
+            j += sign
+            if j == n - (n - 1):
+                sign -= 1
+                j += 2
+            continue
+        ret_str += item[x]
+        print(item[x])
+    ret_str += '-' * count
+    ret_lis.append(ret_str)
+    print(ret_str)
+    ret_str = ''
+    count += 2
+print(alpha[index])
+print(ret_lis)
+print(lis)
 
 #  the method that im going to use is like this :
 # first im gonna fill all the blocks with hyphens then .... see you tomorrow
